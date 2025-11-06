a4a162daf25d1f460b7b25d465a0f4160412b1e2
Ayush jha <jhaa97204@gmail.com>
2025-11-06 22:02:45 +0530

    files

--- routing/app/page.js
+++ routing/app/page.js
@@ -3,7 +3,14 @@ export default function Home() {
   return (
     <>
     <h1>Technical Agency</h1>
-     <Link href="/about">about page</Link>
+    <p><Link href="/about">about page</Link></p>
+    <p><Link href="/services">services page</Link></p>
+    <p><Link href="/files">files page</Link></p>
+    <p><Link href="/blogs">blogs page</Link></p>
+     
+     
+     
+     
     </>
 
   );
---
+++ routing/app/services/layout.js
@@ -0,0 +1,9 @@
+export default function Serviceslayout({children}) {
+    return (
+        <div>
+
+            <h3>Services layout</h3>
+            {children}
+        </div>
+    );
+}
